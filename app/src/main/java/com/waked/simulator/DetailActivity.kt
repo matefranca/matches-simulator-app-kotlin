418678e04183e1066b6654e0a75228bf1865c726
Matheus Waked Franca <matheus363@hotmail.com>
2022-06-13 16:09:19 -0300

    Desenvolvimento dos layouts de listagem e detalhe de partidas

---
+++ app/src/main/java/com/waked/simulator/DetailActivity.kt
@@ -0,0 +1,19 @@
+package com.waked.simulator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.waked.simulator.databinding.ActivityDetailBinding
+
+class DetailActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityDetailBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityDetailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+}
